2530c52b8e49fefc0ea2dbdc33ce76d504db3bf6
mindcavity <yusuf.firaaz@gmail.com>
2016-05-24 19:47:05 +1200

    clean up code and files

--- info226/addToDirectory.aspx.cs
+++ info226/addToDirectory.aspx.cs
@@ -19,14 +19,6 @@ public partial class _Default : System.Web.UI.Page
     }
     protected void Submit_Click(object sender, EventArgs e)
     {
-        List<string> newList = new List<string>();
-        TextBox[] textBoxes = { tbCompName, tbBuildName, tbStreetName, tbTown, tbPostCode, tbEmail, tbPhone, tbContactName, tbContactEmail, tbContactPhone, tbLastDate, tbContractor, tbNotes, tbMaintenance};
-
-        for (int i = 0; i < textBoxes.Length; i++)
-        {
-            newList.Add(Convert.ToString(textBoxes[i].Text));
-        }
-
         Response.Redirect("default.aspx");
     }
 }
